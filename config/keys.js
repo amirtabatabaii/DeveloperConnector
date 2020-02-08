b84111ab78ec74cb0620f36e6dfc9a58bf4cd037
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-02-09 00:37:33 +0300

    create token jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Amir:123@mycluster0-lewjb.mongodb.net/DevConnector'
+    mongoURI: 'mongodb+srv://Amir:123@mycluster0-lewjb.mongodb.net/DevConnector',
+    secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require('../../config/keys')
 
 //load User model
 const User = require("../../models/User");
@@ -80,9 +82,28 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({
-              msg: 'Success'
-            })
+            //User Matched
+
+            // create JWT Payload
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            }
+
+            //Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey, {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                })
+              });
+
           } else {
             return res.status(400).json({
               password: 'Password incorrect'

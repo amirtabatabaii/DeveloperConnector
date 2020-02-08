c4a1629a0700c0aff224c83f80df11aad25c6905
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-02-08 23:03:18 +0300

    initial express server with route files

---
+++ routes/api/posts.js
@@ -0,0 +1,14 @@
+// posts
+// ...
+
+const express = require('express');
+const router = express.Router();
+
+// @route    GET api/post/test
+// @desc     Tests posts route
+// @access   Public
+router.get('/test', (req, res) => res.json({
+    msg: "Posts Works"
+}));
+
+module.exports = router;

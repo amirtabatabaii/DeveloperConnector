0db02dee9bbed612a2c09d60b4399ab5824730b9
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-02-08 20:59:09 +0300

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB by mongoose
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Helloaaa!"));
 

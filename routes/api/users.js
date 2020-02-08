4c6cb3c8fe1574f6a76430e3186e1de87da1aba0
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-02-09 00:15:56 +0300

    checking email/password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,4 +58,38 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route    GET api/users/login
+// @desc     login user / returning JWT Token
+// @access   Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find the user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      //Check 4 user
+      if (!user) {
+        return res.status(404).json({
+          email: 'User not found'
+        })
+      }
+      //check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({
+              msg: 'Success'
+            })
+          } else {
+            return res.status(400).json({
+              password: 'Password incorrect'
+            })
+          }
+        })
+    })
+})
+
 module.exports = router;

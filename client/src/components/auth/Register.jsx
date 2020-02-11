1da94b024ada683abb5cb06a37d8f68a430f61c3
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-02-11 19:31:08 +0300

    login form functionality

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -49,7 +49,7 @@ class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">
-                Create your DevConnector account
+                Create your Developer Connector account
               </p>
               <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
